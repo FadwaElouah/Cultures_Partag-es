e922a2b21040c8826bcfca9b54b654d314b9a78c
FadwaElouah <fadwaelouah@gmail.com>
2025-01-02 10:26:07 +0100

    add function

--- Category.php
+++ Category.php
@@ -8,5 +8,30 @@ class Article {
     public function __construct() {
         $this->db = Database::getInstance()->getConnection();
     }
+
+    public function getAllArticles($page = 1, $perPage = 10, $category = null) {
+        $offset = ($page - 1) * $perPage;
+        $sql = "SELECT a.*, c.name as category_name, u.name as author_name 
+                FROM articles a 
+                JOIN categories c ON a.id_categorie = c.id_categorie 
+                JOIN utilisateur u ON a.id_auteur = u.id_utilisateur 
+                WHERE a.status = 'approved'";
+        
+        if ($category) {
+            $sql .= " AND a.id_categorie = ?";
+        }
+        
+        $sql .= " ORDER BY a.created_at DESC LIMIT ? OFFSET ?";
+        
+        $stmt = $this->db->prepare($sql);
+        
+        if ($category) {
+            $stmt->execute([$category, $perPage, $offset]);
+        } else {
+            $stmt->execute([$perPage, $offset]);
+        }
+        
+        return $stmt->fetchAll();
+    }
  
 }

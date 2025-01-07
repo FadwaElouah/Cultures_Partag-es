82966dde546e0c6e4c391f8873c955e4eee8e580
FadwaElouah <fadwaelouah@gmail.com>
2025-01-07 22:20:16 +0100

    add generate pdf

--- Article.php
+++ Article.php
@@ -97,4 +97,27 @@ public function addToFavorites($article_id, $user_id) {
     return $stmt->execute([$article_id, $user_id]);
 }
 
+public function generatePDF($article_id) {
+    require_once('tcpdf/tcpdf.php');
+
+    $article = $this->getArticleById($article_id);
+
+    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    $pdf->SetCreator(PDF_CREATOR);
+    $pdf->SetTitle($article['title']);
+    $pdf->SetHeaderData('', 0, $article['title'], '');
+    $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+    $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+    $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+    $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+    $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+    $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+    $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+    $pdf->SetFont('dejavusans', '', 10);
+    $pdf->AddPage();
+    $pdf->writeHTML($article['content'], true, false, true, false, '');
+
+    return $pdf->Output($article['title'] . '.pdf', 'S');
+}
+
 }
